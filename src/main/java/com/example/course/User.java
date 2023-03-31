317700068c7f9e239272765c07a1676756452876
paulo-zx <paulo-zx@hotmail.com>
2023-03-31 00:24:23 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/example/course/User.java
+++ src/main/java/com/example/course/User.java
@@ -1,6 +1,7 @@
 package com.example.course;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,6 +23,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
