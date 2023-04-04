2fa3f8aa5501d17cd9194b6b07559cf7d9df910f
paulo-zx <paulo-zx@hotmail.com>
2023-04-03 21:27:38 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/Payment.java
+++ src/main/java/com/example/course/Payment.java
@@ -1,5 +1,6 @@
 package com.example.course;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,6 +20,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {
